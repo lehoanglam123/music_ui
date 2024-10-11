3642a9fcf2ec116f9e6a8712961ed7d17239a1b5
lehoanglam123 <lehoanglamit@gmail.com>
2024-10-11 23:04:35 +0700

    continuous Player Bar

--- src/components/PlayerControl/PlayerBar/PlayerBar.js
+++ src/components/PlayerControl/PlayerBar/PlayerBar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
     faBackwardStep,
     faForwardStep,
@@ -15,18 +15,23 @@ import classNames from 'classnames/bind';
 import style from './PlayerBar.module.scss';
 const cx = classNames.bind(style);
 function PlayerBar() {
+    const audioRef = useRef();
+    const [play, setPlay] = useState(false);
     const [showPlay, setShowPlay] = useState(true);
     const [showPause, setShowPause] = useState(false);
 
-    const handlePlay = () => {
-        setShowPause(true);
+    const handlePlayingAudio = () => {
         setShowPlay(false);
+        setShowPause(true);
+        audioRef.current.play();
     };
 
-    const handlePause = () => {
+    const handleStopAudio = () => {
         setShowPlay(true);
         setShowPause(false);
+        audioRef.current.pause();
     };
+
     return (
         <div className={cx('player-bar')}>
             <div className={cx('player-items')}>
@@ -43,7 +48,10 @@ function PlayerBar() {
                     />
                 </button>
                 {showPlay && (
-                    <button className={cx('control-btn')} onClick={handlePlay}>
+                    <button
+                        className={cx('control-btn')}
+                        onClick={handlePlayingAudio}
+                    >
                         <FontAwesomeIcon
                             className={cx('control-item', 'item-play')}
                             icon={faCirclePlay}
@@ -51,7 +59,10 @@ function PlayerBar() {
                     </button>
                 )}
                 {showPause && (
-                    <button className={cx('control-btn')} onClick={handlePause}>
+                    <button
+                        className={cx('control-btn')}
+                        onClick={handleStopAudio}
+                    >
                         <FontAwesomeIcon
                             className={cx('control-item', 'item-play')}
                             icon={faCirclePause}
@@ -83,6 +94,12 @@ function PlayerBar() {
                 </div>
                 <span className={cx('time-left')}>03:40</span>
             </div>
+            <audio
+                ref={audioRef}
+                src={
+                    'https://res.cloudinary.com/dcyemfp6o/video/upload/v1713276342/Music/Vietnam/Ch%C3%BAng%20ta%20c%E1%BB%A7a%20hi%E1%BB%87n%20t%E1%BA%A1i.mp3'
+                }
+            />
         </div>
     );
 }
